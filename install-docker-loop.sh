3fb1409df8bbf482d067b29edfae961144582cf4
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-04-08 05:17:02 +0000

    minor fix

--- install-docker-loop.sh
+++ install-docker-loop.sh
@@ -4,6 +4,6 @@ mkdir -p "logs"
 while true;
 do
 	RUN="$RANDOM$RANDOM"
-	echo "[$(date -R)] Starting run $RUN"
+	echo "[$(date -R)] Starting run#$RUN"
 	./install-docker.sh 2>&1 | tee "logs/$RUN.log"
 done
